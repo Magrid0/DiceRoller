78689978132b62ae85b172a006141e5bae9c5d6a
Magrid <giu007m@gmail.com>
2023-08-01 17:48:33 +0200

    - "Roller" Class created. - Imported "Java.utils.Random"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Random; // Import the random module.
+
 public class Main {
 
+    Random random = new Random(); // Create an instance of Random class.
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        System.out.println("Dice Roller by Magrid0");
+    } // Welcome the user.
 
 }
---
+++ src/Roller.java
@@ -0,0 +1,40 @@
+// Roller class, it handles the generation of random numbers.
+public class Roller {
+
+    public static void d6 () {
+
+
+
+    }
+
+    public static void d8 () {
+
+
+
+    }
+
+    public static void d10 () {
+
+
+
+    }
+
+    public static void d12 () {
+
+
+
+    }
+
+    public static void d20 () {
+
+
+
+    }
+
+    public static void d30 () {
+
+
+
+    }
+
+}
