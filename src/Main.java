18eb54e5ed3ab1d31c290200fa63e00fbbff0b35
Magrid <giu007m@gmail.com>
2023-08-01 20:02:34 +0200

    Major bug fixed.

--- src/Main.java
+++ src/Main.java
@@ -7,50 +7,52 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Dice Roller by Magrid0");
-    } // Welcome the user.
 
-    // Initialize scanner.
-    Scanner scanner = new Scanner(System.in);
+        // Initialize scanner.
+        Scanner scanner = new Scanner(System.in);
 
-    boolean exit = false; // Declare exit variable.
+        System.out.println("Dice Roller by Magrid0"); // Welcome the user.
 
-    while (!exit) {
+        boolean exit = false; // Declare exit variable.
 
-        // Choose the dice.
-        System.out.println("[1] Six Faces (d6)");
-        System.out.println("[2] Eight Faces (d8)");
-        System.out.println("[3] Ten Faces (d10)");
-        System.out.println("[4] Twelve Faces (d12)");
-        System.out.println("[5] Twenty Faces (d20)");
-        System.out.println("[6] Thirty Faces (d30)");
-        System.out.println("[7] Exit");
-        System.out.print("Select a type of dice: ");
-        GlobalVariables.choice = Integer.parseInt(scanner.nextLine());
+        while (!exit) {
 
-        // Validate the user's choice.
-        while (GlobalVariables.choice < 1 || GlobalVariables.choice > 7) {
-            System.out.println("Invalid choice. Please enter a valid choice (1-7).");
+            // Choose the dice.
+            System.out.println("[1] Six Faces (d6)");
+            System.out.println("[2] Eight Faces (d8)");
+            System.out.println("[3] Ten Faces (d10)");
+            System.out.println("[4] Twelve Faces (d12)");
+            System.out.println("[5] Twenty Faces (d20)");
+            System.out.println("[6] Thirty Faces (d30)");
+            System.out.println("[7] Exit");
+            System.out.print("Select a type of dice: ");
             GlobalVariables.choice = Integer.parseInt(scanner.nextLine());
-        }
 
-        switch (GlobalVariables.choice) {
+            // Validate the user's choice.
+            while (GlobalVariables.choice < 1 || GlobalVariables.choice > 7) {
+                System.out.println("Invalid choice. Please enter a valid choice (1-7).");
+                GlobalVariables.choice = Integer.parseInt(scanner.nextLine());
+            }
 
-            case 1 -> Roller.d6();
+            switch (GlobalVariables.choice) {
 
-            case 2 -> Roller.d8();
+                case 1 -> Roller.d6();
 
-            case 3 -> Roller.d10();
+                case 2 -> Roller.d8();
 
-            case 4 -> Roller.d12();
+                case 3 -> Roller.d10();
 
-            case 5 -> Roller.d20();
+                case 4 -> Roller.d12();
 
-            case 6 -> Roller.d30();
+                case 5 -> Roller.d20();
 
-            case 7 -> exit = true;
+                case 6 -> Roller.d30();
 
-            default -> System.out.println("Invalid choice");
+                case 7 -> exit = true;
+
+                default -> System.out.println("Invalid choice");
+
+            }
 
         }
 
