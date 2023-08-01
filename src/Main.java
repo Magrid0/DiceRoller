bd5c8369f0b67c57f34be5f3fd47087d01813a28
Magrid <giu007m@gmail.com>
2023-08-01 18:16:07 +0200

    - Roller.java is finished. - Main.java need to be fixed.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,62 @@
-import java.util.Random; // Import the random module.
+import java.util.Scanner;
 
 public class Main {
 
-    Random random = new Random(); // Create an instance of Random class.
+    static class GlobalVariables { // Declare global variables.
+        public static int choice;
+    }
 
     public static void main(String[] args) {
         System.out.println("Dice Roller by Magrid0");
     } // Welcome the user.
+
+    // Initialize scanner.
+    Scanner scanner = new Scanner(System.in);
+
+    boolean exit = false; // Declare exit variable.
+
+    while (!exit) {
+
+        // Choose the dice.
+        System.out.println("[1] Six Faces (d6)");
+        System.out.println("[2] Eight Faces (d8)");
+        System.out.println("[3] Ten Faces (d10)");
+        System.out.println("[4] Twelve Faces (d12)");
+        System.out.println("[5] Twenty Faces (d20)");
+        System.out.println("[6] Thirty Faces (d30)");
+        System.out.println("[7] Exit");
+        System.out.print("Select a type of dice: ");
+        GlobalVariables.choice = Integer.parseInt(scanner.nextLine());
+
+        // Validate the user's choice.
+        while (GlobalVariables.choice < 1 || GlobalVariables.choice > 7) {
+            System.out.println("Invalid choice. Please enter a valid choice (1-7).");
+            GlobalVariables.choice = Integer.parseInt(scanner.nextLine());
+        }
+
+        switch (GlobalVariables.choice) {
+
+            case 1 -> Roller.d6();
+
+            case 2 -> Roller.d8();
+
+            case 3 -> Roller.d10();
+
+            case 4 -> Roller.d12();
+
+            case 5 -> Roller.d20();
+
+            case 6 -> Roller.d30();
+
+            case 7 -> exit = true;
+
+            default -> System.out.println("Invalid choice");
+
+        }
+
+        // Close scanner.
+        scanner.close();
+
+    }
 
 }
--- src/Roller.java
+++ src/Roller.java
@@ -1,39 +1,79 @@
+import java.util.Random; // Import the random module.
+
 // Roller class, it handles the generation of random numbers.
 public class Roller {
 
+    static Random random = new Random(); // Create an instance of Random class.
+
     public static void d6 () {
 
+        int min = 1; // Minimum value of the range
+        int max = 6; // Maximum value of the range
 
+        // Generate a random number within the range (min to max, inclusive).
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        System.out.println("Random number within the range(1-6): " + randomNumber); // Print the number.
 
     }
 
     public static void d8 () {
 
+        int min = 1; // Minimum value of the range
+        int max = 8; // Maximum value of the range
 
+        // Generate a random number within the range (min to max, inclusive).
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        System.out.println("Random number within the range(1-8): " + randomNumber); // Print the number.
 
     }
 
     public static void d10 () {
 
+        int min = 1; // Minimum value of the range
+        int max = 10; // Maximum value of the range
 
+        // Generate a random number within the range (min to max, inclusive).
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        System.out.println("Random number within the range(1-10): " + randomNumber); // Print the number.
 
     }
 
     public static void d12 () {
 
+        int min = 1; // Minimum value of the range
+        int max = 12; // Maximum value of the range
 
+        // Generate a random number within the range (min to max, inclusive).
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        System.out.println("Random number within the range(1-12): " + randomNumber); // Print the number.
 
     }
 
     public static void d20 () {
 
+        int min = 1; // Minimum value of the range
+        int max = 20; // Maximum value of the range
 
+        // Generate a random number within the range (min to max, inclusive).
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        System.out.println("Random number within the range(1-20): " + randomNumber); // Print the number.
 
     }
 
     public static void d30 () {
 
+        int min = 1; // Minimum value of the range
+        int max = 30; // Maximum value of the range
 
+        // Generate a random number within the range (min to max, inclusive).
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        System.out.println("Random number within the range(1-30): " + randomNumber); // Print the number.
 
     }
 
